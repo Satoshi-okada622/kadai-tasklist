5ce88a2bab9e98e6741ef1fbd3822be840bbd396
satoshi.okada <ubuntu@ip-172-31-10-77.ap-northeast-1.compute.internal>
2021-05-27 09:15:54 +0000

    kadai-tasks

--- database/migrations/2021_05_26_170122_add_title_to_status_table.php
+++ database/migrations/2021_05_26_170122_add_title_to_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTitleToStatus extends Migration
+class AddTitleToStatusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTitleToStatus extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('name', 10);
+            $task->string('title');
         });
     }
 
@@ -26,7 +26,7 @@ class AddTitleToStatus extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumm('name', 10);
+            $table->dropColumn('title');
         });
     }
 }
